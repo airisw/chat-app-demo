521d969b3cd0859fd373bcb0c6feb85fa1551e6d
Airis <airiswang@hotmail.com>
2023-07-25 22:42:09 -0400

    Initial Commit

---
+++ ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by Airis Wang on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
